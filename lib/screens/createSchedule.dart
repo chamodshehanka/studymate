26648d7f86bff2ae90ff5b2db54a06503f5ec863
SuraMahesh <surangamunasinghe22568@gmail.com>
2019-08-01 23:21:35 +0530

    Student Management

--- lib/screens/createSchedule.dart
+++ lib/screens/createSchedule.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import "package:carousel_slider/carousel_slider.dart";
 
 class createSchedule extends StatefulWidget{
   @override
