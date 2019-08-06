27845af430d9121f8528fbdda32482db967807c0
DurekshaDassanayake <50927336+DurekshaDassanayake@users.noreply.github.com>
2019-08-06 15:16:08 +0530

    Add files via upload

---
+++ lib/admin/admin_profile.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class AdminProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AdminProfilePage(),
+    );
+  }
+}
+
+class AdminProfilePage extends StatefulWidget {
+  @override
+  _AdminProfilePage createState() => _AdminProfilePage();
+}
+
+class _AdminProfilePage extends State<AdminProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(56, 52, 175, 1.0),
+          title: Text(
+            'MY ACCOUNT',
+            style: TextStyle(fontWeight: FontWeight.normal),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => MyApp()),
+              // );
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                color: const Color.fromRGBO(56, 52, 175, 1.0),
+                // padding: EdgeInsets.only(
+                // top: 20.0, right: 20.0, left: 20.0, bottom: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 40.0,
+                          ),
+                          Text(
+                            'Thilini Dureksha',
+                            style: TextStyle(
+                              color: Colors.white70,
+                              fontSize: 17,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          buildButtonContainer(),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                            child: Container(
+                              color: Colors.indigoAccent[100],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget buildButtonContainer() {
+    return Container(
+      height: 45.0,
+      width: 250,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.indigoAccent[100],
+      ),
+      child: Center(
+        child: Text(
+          'EDIT PROFILE',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,185 @@
 import 'package:flutter/material.dart';
-import 'package:studymate/screens/SplashScreen.dart';
-import 'package:studymate/screens/createSchedule.dart';
-import 'package:studymate/screens/daily.dart';
-import 'package:studymate/screens/first.dart';
-import 'package:studymate/screens/home.dart';
-import 'package:studymate/screens/signin.dart';
-import 'package:studymate/screens/signup.dart';
-import 'package:studymate/screens/Appointment.dart';
-import 'package:studymate/screens/subject.dart';
+import './parent/parent_register.dart';
 
-import 'screens/accounttype.dart';
-import 'screens/add.dart';
-import 'screens/signup1.dart';
+main() {
+  runApp(MyApp());
+}
 
-var routes = <String, WidgetBuilder>{
-  '/first': (context) => WelcomeScreen(),
-  '/signin': (context) => SignInScreen(),
-  '/signup': (context) => SignUpScreen(),
-  '/home': (context) => HomeScreen(),
-  '/subject': (context) => SubjectScreen(),
-  '/signup1': (context) => SignUp1Screen(),
-  '/accounttype': (context) => AccountTypeScreen(),
-  '/add': (context) => AddScreen(),
-  '/daily': (context) => DailyScreen(),
-  '/Appointment': (context) => MyApp(),
-  '/createSchedule': (context) => createSchedule()
-};
-
-void main() {
-  var materialApp = MaterialApp(
-      title: 'StudyMate',
-      theme: ThemeData(
-          primaryColor: new Color(0xff622F74), accentColor: Colors.deepPurple),
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-      routes: routes);
-  return runApp(materialApp);
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(56, 52, 175, 1.0),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(
+                    top: 100.0, right: 20.0, left: 20.0, bottom: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset('assets/logo.png'),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    buildUsernameField(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    buildPasswordField(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Forgot Password ?',
+                            style: TextStyle(
+                              color: Colors.indigo[50],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    buildButtonContainer(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Don\'t have an account ?',
+                              style: TextStyle(
+                                color: Colors.white70,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ParentRegister()),
+                                );
+                              },
+                              child: Text(
+                                'SIGN UP',
+                                style: TextStyle(
+                                  color: Colors.indigo[50],
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+
+  Widget buildUsernameField() {
+    return TextField(
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      decoration: InputDecoration(
+        labelText: 'Username',
+        labelStyle: TextStyle(
+          color: Colors.lightBlue[200],
+          fontSize: 16.0,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.lightBlue[400], width: 2.0),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.lightBlue[400], width: 2.0),
+        ),
+        prefixIcon: Icon(
+          Icons.email,
+          color: Colors.lightBlue[400],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPasswordField() {
+    return TextField(
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      decoration: InputDecoration(
+          labelText: 'Password',
+          labelStyle: TextStyle(
+            color: Colors.lightBlue[200],
+            fontSize: 16.0,
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.lightBlue[400], width: 2.0),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.lightBlue[400], width: 2.0),
+          ),
+          prefixIcon: Icon(
+            Icons.lock,
+            color: Colors.lightBlue[400],
+          )),
+      obscureText: true,
+    );
+  }
+
+  Widget buildButtonContainer() {
+    return Container(
+      height: 56.0,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.indigoAccent[100],
+      ),
+      child: Center(
+        child: Text(
+          'LOGIN',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
