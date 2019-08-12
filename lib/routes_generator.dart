802deaba41f749d855979d8363df826c5c7547c3
Shehanka <hcsperera@outlook.com>
2019-08-13 01:29:44 +0530

    Badge link error fixed

--- lib/routes_generator.dart
+++ lib/routes_generator.dart
@@ -8,7 +8,7 @@ import 'package:studymate/screens/createSchedule.dart';
 import 'package:studymate/screens/daily.dart';
 import 'package:studymate/screens/first.dart';
 import 'package:studymate/screens/home.dart';
-// import 'package:studymate/screens/rewards.dart';
+import 'package:studymate/screens/rewards.dart';
 import 'package:studymate/screens/signin.dart';
 import 'package:studymate/screens/signup.dart';
 import 'package:studymate/screens/signup1.dart';
@@ -38,8 +38,8 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => AppointmentScreen());
       case '/badge':
         return MaterialPageRoute(builder: (_) => BadgesScreen());
-      // case '/rewards'
-      //   return MaterialPageRoute(builder: (_) => RewardsScreen());
+      case '/rewards':
+        return MaterialPageRoute(builder: (_) => RewardsScreen());
       case 'createSchedule':
         return MaterialPageRoute(builder: (_) => CreateSchedule());
       default:
