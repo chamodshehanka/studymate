f2fb9d9a1623975fe06d213dd4b9085801fdebb3
heshanmark <heshanmark@users.noreply.github.com>
2019-08-14 17:28:14 +0530

    MedicalRecords UI file created

---
+++ lib/screens/MedicalRecordsScreen/MedicalRecordUI.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MedicalRecordUIScreen extends StatefulWidget{
+  _MedicalRecordScreenState createState() => _MedicalRecordScreenState();
+}
+
+class _MedicalRecordScreenState extends State<MedicalRecordUIScreen>{
+  @override
+  Widget build(BuildContext context) {
+    
+    return null;
+  }
+
+}
