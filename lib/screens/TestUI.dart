d9522d0b3967b55cbad22c8ce06db4300f855034
Shehanka <hcsperera@outlook.com>
2019-08-15 14:07:33 +0530

    Test UI created

---
+++ lib/screens/TestUI.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class TestUIScreen extends StatefulWidget {
+  _TestUIScreenState createState() => _TestUIScreenState();
+}
+
+class _TestUIScreenState extends State<TestUIScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
