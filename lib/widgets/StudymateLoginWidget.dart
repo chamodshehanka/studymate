2c4c688be106591692ef6a451cfa79eeb02cc871
Shehanka <hcsperera@outlook.com>
2019-08-15 10:59:55 +0530

    New Login UI making

---
+++ lib/widgets/StudymateLoginWidget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StudymateLoginWidget extends StatefulWidget {
+  _StudymateLoginWidgetState createState() => _StudymateLoginWidgetState();
+}
+
+class _StudymateLoginWidgetState extends State<StudymateLoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+  
+}
