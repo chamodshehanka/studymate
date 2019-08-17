ca5d20d256a7cea75f2731c307813da5ea86f86c
DurekshaDassanayake <50927336+DurekshaDassanayake@users.noreply.github.com>
2019-08-17 22:05:18 +0530

    User Management Screens v2

---
+++ lib/screens/HomeScreen/HomeUIAdmin.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:studymate/widgets/DrawerTile.dart';
+import 'package:studymate/widgets/HomeTile.dart';
+
+class HomeScreenAdmin extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreenAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        backgroundColor: Colors.white,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Icon(
+                  Icons.account_circle,
+                  size: 90.0,
+                ),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                  Colors.deepOrange,
+                  Colors.orangeAccent,
+                  Colors.yellow,
+                ])),
+              ),
+              DrawerTile(Icons.person, 'Profile', () => {}),
+              DrawerTile(Icons.note, 'Reminders', () => {}),
+              DrawerTile(Icons.info, 'About Us', () => {}),
+              DrawerTile(Icons.settings, 'Settings', () => {}),
+              DrawerTile(Icons.exit_to_app, 'Logout', () => {}),
+            ],
+          ),
+        ),
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20.0),
+          crossAxisSpacing: 10.0,
+          crossAxisCount: 2,
+          children: <Widget>[
+            HomeTile(Icons.verified_user, "User Manager", Colors.red, "/userManagementAdmin"),
+            HomeTile(Icons.calendar_today, "Appointments", Colors.amber, ""),
+            HomeTile(Icons.local_activity, "Activity Manager", Colors.cyan, ""),
+            HomeTile(Icons.book, "Subject Manager", Colors.lightBlue, ""),
+            HomeTile(Icons.calendar_today, "Schedules", Colors.cyan, ""),
+            HomeTile(Icons.supervised_user_circle, "Profile", Colors.red, "")
+          ],
+        ));
+  }
+}
