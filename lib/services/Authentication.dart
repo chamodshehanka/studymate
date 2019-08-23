5d3383d9aaadbc9d5b8f296c35c13bbc9bdaa233
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-08-23 15:28:38 +0530

    Firebase Auth adding

--- lib/services/Authentication.dart
+++ lib/services/Authentication.dart
@@ -1,3 +1,39 @@
-class Authentication {
-  
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class BaseAuthentication {
+  Future<String> signIn(String email, String password);
+  Future<String> signUp(String email, String password);
+  Future<String> getCurrentUser();
+  Future<void> signOut();
 }
+
+class Authentication implements BaseAuthentication {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  
+  @override
+  Future<String> getCurrentUser() async {
+    FirebaseUser user = await _firebaseAuth.currentUser();
+    return user.uid;
+  }
+
+  @override
+  Future<String> signIn(String email, String password) async {
+    FirebaseUser user = (await 
+    _firebaseAuth.signInWithEmailAndPassword(email: email, password: password)) as FirebaseUser;
+    return user.uid;
+  }
+
+  @override
+  Future<void> signOut() {
+    return _firebaseAuth.signOut();
+  }
+
+  @override
+  Future<String> signUp(String email, String password) async {
+    FirebaseUser user = (await
+    _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password)) as FirebaseUser;
+    return user.uid;
+  }
+}
+
+// https://medium.com/flutterpub/flutter-how-to-do-user-login-with-firebase-a6af760b14d5
