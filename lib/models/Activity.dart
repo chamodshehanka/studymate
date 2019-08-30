f053fa18b4b42e9ff835bcdbfd9f406033caefba
Shehanka <hcsperera@outlook.com>
2019-08-30 17:10:26 +0530

    DB classes

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -1,16 +1,13 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-
 class Activity {
   String id;
   String title;
   String time;
   Activity(this.title, this.time);
 
-  List getActivities() {
-    // CollectionReference activityRef = Firestore.instance.collection('activity');
-    // Future<QuerySnapshot> eventQuery = activityRef.getDocuments();
+  // Activity.fromSnapshot(DataSnapshot snapshot) : 
 
-    // return documents;
+
+  List getActivities() {
     return [
       Activity("eventQuery.toString()", "9.45 - 10.00"),
       Activity("Play A Sport", "17.00 - 18.00"),
