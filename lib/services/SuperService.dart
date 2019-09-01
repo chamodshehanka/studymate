a137191adaa8310846621aeacf0bbc01e6facb72
Chamod Shehanka Perera <hcsperera@outlook.com>
2019-09-01 20:34:26 +0530

    Serivice factory making

--- lib/services/SuperService.dart
+++ lib/services/SuperService.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class SuperService<T, ID> {
   Future<T> create(T t);
-  
+
   Future<dynamic> update(T t);
 
   Future<dynamic> remove(ID id);
