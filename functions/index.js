97a2900d8d502915c20fde4077aa5a52177b70cb
Shehanka <hcsperera@outlook.com>
2019-09-17 00:12:04 +0530

    Firebase new role cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,5 +14,21 @@ const functions = require('firebase-functions');
 // exports.newActivityNotification = functions.firestore
 //     .document('Activities/{id}')
 //     .onCreate((change, context) => {
-        
+
 //     });
+
+// New SignUp
+exports.addNewRole = functions.https.onCall((data, context) => {
+    // Get user and add custom claim (admin)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true
+        });
+    }).then(() => {
+        return {
+            message: `Admin added Success! ${data.email}`
+        }
+    }).catch(err => {
+        return err;
+    })
+});
