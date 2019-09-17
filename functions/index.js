11ab650ff1dc25168402322ea9237c0b8198c179
Shehanka <hcsperera@outlook.com>
2019-09-17 11:12:16 +0530

    Powershell bypass added

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,8 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+// Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+// 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
