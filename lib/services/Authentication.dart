27af4765423bf1a85be1f732ddd1861269eb0ba1
Shehanka <hcsperera@outlook.com>
2019-09-19 01:26:10 +0530

    Splashscreen update

--- lib/services/Authentication.dart
+++ lib/services/Authentication.dart
@@ -32,7 +32,8 @@ class Authentication implements BaseAuthentication {
   @override
   Future<String> signUp(String email, String password) async {
     FirebaseUser user = (await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password)) as FirebaseUser;
+            email: email, password: password))
+        .user;
     return user.uid;
   }
 }
