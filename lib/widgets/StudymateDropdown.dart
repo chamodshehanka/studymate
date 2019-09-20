ad5afc36bb5bb5215f9ecc1480c97988681aea86
Shehanka <hcsperera@outlook.com>
2019-09-20 23:32:24 +0530

    Optimized

--- lib/widgets/StudymateDropdown.dart
+++ lib/widgets/StudymateDropdown.dart
@@ -1,56 +1,50 @@
 import 'package:flutter/material.dart';
 
 class StudymateDropdown extends StatefulWidget {
+  final List<String> valueList;
+  final String labelText;
 
-    
-final List<String> valueList;
-final String labelText;
+  StudymateDropdown(this.labelText, this.valueList);
 
-
-StudymateDropdown(this.labelText,this.valueList);
-
-  
- _StudymateDropdownState createState() => _StudymateDropdownState();
-
+  _StudymateDropdownState createState() => _StudymateDropdownState();
 }
 
 
+  getVal() {}
 class _StudymateDropdownState extends State<StudymateDropdown> {
   String selectedValue;
-@override
-Widget build(BuildContext context) {
-  return Container(
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       width: 350,
       child: Padding(
-        padding: EdgeInsets.only(bottom: 10.0,left: 20.0,top: 10.0,right: 20.0),
+        padding:
+            EdgeInsets.only(bottom: 10.0, left: 20.0, top: 10.0, right: 20.0),
         child: DropdownButton<String>(
-        hint: Text(widget.labelText),
-        value: selectedValue,
-        icon: Icon(Icons.arrow_drop_down),
-        iconSize: 24,
-        isExpanded: true,
-        elevation: 16,
-        style: TextStyle(
-          color: Colors.deepPurple
-        ),
-        underline: Container(
-          height: 2,
-          color: Colors.deepPurpleAccent,
-        ),
-        onChanged: (String newValue) {
-          setState(() {
-            selectedValue = newValue;
-          });
-        },
-        items: widget.valueList.map<DropdownMenuItem<String>>((String value) {
+          hint: Text(widget.labelText),
+          value: selectedValue,
+          icon: Icon(Icons.arrow_drop_down),
+          iconSize: 24,
+          isExpanded: true,
+          elevation: 16,
+          style: TextStyle(color: Colors.deepPurple),
+          underline: Container(
+            height: 2,
+            color: Colors.deepPurpleAccent,
+          ),
+          onChanged: (String newValue) {
+            setState(() {
+              selectedValue = newValue;
+            });
+          },
+          items: widget.valueList.map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
               child: Text(value),
             );
-          })
-          .toList(),
+          }).toList(),
+        ),
       ),
-  ),
-  );
-}
+    );
+  }
 }
--- lib/widgets/StudymateTextField.dart
+++ lib/widgets/StudymateTextField.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class StudymateTextField extends StatefulWidget {
-  
   final String labelText;
   final TextEditingController textEditingController;
   final String validation;
   final Color color;
   final TextInputType keyboardType;
 
-  StudymateTextField(this.labelText,this.textEditingController,this.validation,this.color,this.keyboardType);
+  StudymateTextField(this.labelText, this.textEditingController,
+      this.validation, this.color, this.keyboardType);
 
   _StudymateTextField createState() => _StudymateTextField();
 }
@@ -17,35 +17,31 @@ class _StudymateTextField extends State<StudymateTextField> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 350,
-    
-      child: 
-      Padding(
-        padding: EdgeInsets.only(bottom: 10.0,left: 20.0,top: 10.0,right: 20.0),
-        child: TextFormField(
-                      decoration: new InputDecoration(
-                        labelText: widget.labelText,
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(
-                          ),
-                        ),
-                      ),
-                      validator: (val) {
-                        if(val.length==0) {
-                          return widget.validation;
-                        }else{
-                          return null;
-                        }
-                      },
-                      keyboardType: widget.keyboardType,
-                      style: new TextStyle(
-                        fontFamily: "Poppins",
-                      ),
-                    ),
-      )
-      
-    );
+        width: 350,
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: 10.0, left: 20.0, top: 10.0, right: 20.0),
+          child: TextFormField(
+            decoration: new InputDecoration(
+              labelText: widget.labelText,
+              fillColor: Colors.white,
+              border: new OutlineInputBorder(
+                borderRadius: new BorderRadius.circular(25.0),
+                borderSide: new BorderSide(),
+              ),
+            ),
+            validator: (val) {
+              if (val.length == 0) {
+                return widget.validation;
+              } else {
+                return null;
+              }
+            },
+            keyboardType: widget.keyboardType,
+            style: new TextStyle(
+              fontFamily: "Poppins",
+            ),
+          ),
+        ));
   }
 }
