b1580cc35155fd88d400800b36e679d5dcc5c95c
Shehanka <hcsperera@outlook.com>
2019-09-22 19:11:07 +0530

    Settings and Activity Menu class Created

---
+++ lib/screens/ActivityScreen/ActivityMenuUI.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ActivityMenuScreen extends StatefulWidget {
+  _ActivityMenuScreenState createState() => _ActivityMenuScreenState();
+}
+
+class _ActivityMenuScreenState extends State<ActivityMenuScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
