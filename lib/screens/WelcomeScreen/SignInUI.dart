81ba342e185a1018030b236400607f70f5ca94e7
Shehanka <hcsperera@outlook.com>
2019-09-24 19:18:39 +0530

    Sign Optmized

--- lib/screens/WelcomeScreen/SignInUI.dart
+++ lib/screens/WelcomeScreen/SignInUI.dart
@@ -61,113 +61,109 @@ class _SignInScreenState extends State<SignInScreen> {
         body: Form(
           key: _formKey,
           child: Stack(
+            alignment: Alignment.topLeft,
             children: <Widget>[
-              Stack(
-                alignment: Alignment.topLeft,
+              ListView(
                 children: <Widget>[
-                  ListView(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 70.0, bottom: 10.0, left: 10.0, right: 10.0),
-                        child: Text(
-                          "Sign In",
-                          softWrap: true,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.deepPurpleAccent,
-                            decoration: TextDecoration.none,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.w700,
-                            fontFamily: "OpenSans",
-                          ),
-                        ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 70.0, bottom: 10.0, left: 10.0, right: 10.0),
+                    child: Text(
+                      "Sign In",
+                      softWrap: true,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.deepPurpleAccent,
+                        decoration: TextDecoration.none,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: "OpenSans",
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 20.0, bottom: 10.0, left: 15.0, right: 15.0),
-                        child: _emailField,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: 10.0, bottom: 20.0, left: 15.0, right: 15.0),
-                        child: _passwordField,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 14.0, horizontal: 40.0),
-                        child: StudymateFlatButton(
-                          title: "Log In",
-                          fontSize: 22,
-                          fontWeight: FontWeight.w700,
-                          textColor: Colors.white,
-                          onPressed: () {
-                            _userLogin();
-                          },
-                          splashColor: Colors.black12,
-                          borderColor: Colors.white,
-                          borderWidth: 0,
-                          color: Colors.deepPurpleAccent,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(
-                          "OR",
-                          softWrap: true,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.black,
-                            decoration: TextDecoration.none,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w300,
-                            fontFamily: "OpenSans",
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 14.0, horizontal: 40.0),
-                        child: StudymateFlatButton(
-                          title: "Admin Login",
-                          fontSize: 22,
-                          fontWeight: FontWeight.w700,
-                          textColor: Colors.white,
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/homeAdmin');
-                          },
-                          splashColor: Colors.black12,
-                          borderColor: Color.fromRGBO(59, 89, 152, 1.0),
-                          borderWidth: 0,
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 14.0, horizontal: 40.0),
-                        child: StudymateFlatButton(
-                          title: "Doctor Login",
-                          fontSize: 22,
-                          fontWeight: FontWeight.w700,
-                          textColor: Colors.white,
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/homeDoctor');
-                          },
-                          splashColor: Colors.black12,
-                          borderColor: Color.fromRGBO(59, 89, 152, 1.0),
-                          borderWidth: 0,
-                          color: Color.fromRGBO(59, 89, 152, 1.0),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                  SafeArea(
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back),
-                      onPressed: onBackPress,
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 20.0, bottom: 10.0, left: 15.0, right: 15.0),
+                    child: _emailField,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        top: 10.0, bottom: 20.0, left: 15.0, right: 15.0),
+                    child: _passwordField,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14.0, horizontal: 40.0),
+                    child: StudymateFlatButton(
+                      title: "Log In",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        _userLogin();
+                      },
+                      splashColor: Colors.black12,
+                      borderColor: Colors.white,
+                      borderWidth: 0,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      "OR",
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: "OpenSans",
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14.0, horizontal: 40.0),
+                    child: StudymateFlatButton(
+                      title: "Admin Login",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/homeAdmin');
+                      },
+                      splashColor: Colors.black12,
+                      borderColor: Color.fromRGBO(59, 89, 152, 1.0),
+                      borderWidth: 0,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14.0, horizontal: 40.0),
+                    child: StudymateFlatButton(
+                      title: "Doctor Login",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/homeDoctor');
+                      },
+                      splashColor: Colors.black12,
+                      borderColor: Color.fromRGBO(59, 89, 152, 1.0),
+                      borderWidth: 0,
+                      color: Color.fromRGBO(59, 89, 152, 1.0),
                     ),
                   ),
                 ],
+              ),
+              SafeArea(
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: onBackPress,
+                ),
               ),
               Offstage(
                 offstage: !_blackVisible,
