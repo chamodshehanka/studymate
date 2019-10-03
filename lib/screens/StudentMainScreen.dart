f86506a8c1f8e55c0b66fad3eedfbffa85764cf9
Shehanka <hcsperera@outlook.com>
2019-10-03 13:33:15 +0530

    Preffered activty adding and deletion working with current user id

--- lib/screens/StudentMainScreen.dart
+++ lib/screens/StudentMainScreen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:studymate/screens/StudentPlaceHolder.dart';
 
 class StudentMainScreen extends StatefulWidget {
- @override
- State<StatefulWidget> createState() {
+  @override
+  State<StatefulWidget> createState() {
     return _StudentMainScreenState();
   }
 }
@@ -13,30 +13,30 @@ class _StudentMainScreenState extends State<StudentMainScreen> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     StudentPlaceholder(0),
-   StudentPlaceholder(1),
-   StudentPlaceholder(2),
-   StudentPlaceholder(3),
+    StudentPlaceholder(1),
+    StudentPlaceholder(2),
+    StudentPlaceholder(3),
   ];
-@override
- Widget build(BuildContext context) {
-   return Scaffold(
-     body: _children[_currentIndex], // new
-    bottomNavigationBar: CurvedNavigationBar(
-  color: Colors.deepPurple,
-  buttonBackgroundColor: Colors.deepPurple,
-    backgroundColor: Colors.white,
-    items: <Widget>[
-      Icon(Icons.home, size: 30,color: Colors.white),
-      Icon(Icons.calendar_today, size: 30,color: Colors.white),
-      Icon(Icons.favorite, size: 30,color: Colors.white),
-      Icon(Icons.person, size: 30,color: Colors.white),
-    ],
-    onTap: (index) {
-     setState(() {
-     _currentIndex = index;
-   });
-    },
-  ),
-   );
- }
- }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex], // new
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.deepPurple,
+        buttonBackgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.white,
+        items: <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.calendar_today, size: 30, color: Colors.white),
+          Icon(Icons.favorite, size: 30, color: Colors.white),
+          Icon(Icons.person, size: 30, color: Colors.white),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
