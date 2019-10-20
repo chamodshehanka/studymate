8155f5a29faa5d7292fa53c41b38081c8d21887e
Shehanka <hcsperera@outlook.com>
2019-10-20 17:01:21 +0530

    Reward dashboard added

---
+++ lib/screens/RewardScreen/RewardDashboardScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RewardDashboardScreen extends StatefulWidget {
+  _RewardDashboardScreenState createState() => _RewardDashboardScreenState();
+}
+
+class _RewardDashboardScreenState extends State<RewardDashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
