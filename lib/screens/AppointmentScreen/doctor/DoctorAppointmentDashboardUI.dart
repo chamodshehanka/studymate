21103473dc52240719a751f347d96f9e1da39163
Shehanka <hcsperera@outlook.com>
2019-10-31 22:16:07 +0530

    Appointment ui

--- lib/screens/AppointmentScreen/doctor/DoctorAppointmentDashboardUI.dart
+++ lib/screens/AppointmentScreen/doctor/DoctorAppointmentDashboardUI.dart
@@ -14,6 +14,7 @@ class _DoctorAppointmentDashboardScreenState
         title: Text('Appointment Dashboard'),
         backgroundColor: Colors.deepPurpleAccent,
       ),
+      body: Container(),
     );
   }
 }
