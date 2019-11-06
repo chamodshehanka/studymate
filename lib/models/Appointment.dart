367c163568f5c6c6fdebf03f8930970c3d814d66
Shehanka <hcsperera@outlook.com>
2019-11-06 19:10:27 +0530

    Not approved appointment ui almost over

--- lib/models/Appointment.dart
+++ lib/models/Appointment.dart
@@ -1,55 +1,50 @@
-//Create Model class to 
-//task parameters
 class Appointment {
   String _id;
-  String _specialDescription;
+  String _description;
   String _date;
   String _time;
   String _place;
-  //String _status;
+  bool _isApproved;
 
-//create constructors
-  Appointment(this._id, this._specialDescription,this._date,this._time,this._place,/*this._status */);
-//add methods like from map to map ,reason that doing is data that 
-//that recieve from the firestore has to be deserialize so that we are using
-//this method **add those property value
+  Appointment(this._id, this._description, this._date, this._time, this._place,
+      this._isApproved);
+
   Appointment.map(dynamic appointment) {
     this._id = appointment['id'];
-    this._specialDescription = appointment['specialDescription'];
+    this._description = appointment['description'];
     this._date = appointment['date'];
     this._time = appointment['time'];
     this._place = appointment['place'];
-    //this._status = appointment['status'];
+    this._isApproved = appointment['isApproved'];
   }
-//we are create getter methods
+
   String get id => _id;
-  String get specialDescription => _specialDescription;
+  String get description => _description;
   String get date => _date;
   String get time => _time;
   String get place => _place;
-//String get status => _status;
+  bool get isApproved => _isApproved;
 
-//deserialized the data recieve from the firestore
   Map<String, dynamic> toMap() {
     var map = new Map<String, dynamic>();
     if (_id != null) {
       map['id'] = _id;
     }
-    map['specialDescription'] = _specialDescription;
+    map['description'] = _description;
     map['date'] = _date;
     map['time'] = _time;
     map['place'] = _place;
-   // map['status'] = _status;
+    map['isApproved'] = _isApproved;
 
     return map;
   }
 
   Appointment.fromMap(Map<String, dynamic> map) {
     this._id = map['id'];
-    this._specialDescription = map['specialDescription'];
+    this._description = map['description'];
     this._date = map['date'];
     this._time = map['time'];
     this._place = map['place'];
-   // this._status = map['status'];
+    this._isApproved = map['isApproved'];
   }
 }
