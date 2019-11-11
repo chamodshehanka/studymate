2eadfe8e4c8e666aca53df9041ee5555c887176a
Shehanka <hcsperera@outlook.com>
2019-11-11 18:30:34 +0530

    Appointment create

--- lib/screens/AppointmentScreen/doctor/utils/AppointmentDialog.dart
+++ lib/screens/AppointmentScreen/doctor/utils/AppointmentDialog.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:studymate/models/Doctor.dart';
 import 'package:studymate/services/custom/AppointmentService.dart';
+import 'package:studymate/services/custom/DoctorService.dart';
 import 'package:studymate/widgets/StudymateTextField.dart';
 
 class AppointmentDialog extends StatefulWidget {
@@ -17,6 +22,38 @@ class _AppointmentDialog extends State<AppointmentDialog> {
   var doctorName;
   var studentName;
   AppointmentService _appointmentService = AppointmentService();
+  DoctorService _doctorService = DoctorService();
+  StreamSubscription<QuerySnapshot> doctorsSubscription;
+  List<Doctor> doctorsList;
+  List<String> doctorNamesList;
+
+  @override
+  void initState() {
+    super.initState();
+
+    doctorNamesList = List();
+    doctorsList = List();
+    doctorsSubscription?.cancel();
+    doctorsSubscription =
+        _doctorService.getAll().listen((QuerySnapshot snapshot) {
+      final List<Doctor> doctors =
+          snapshot.documents.map((doc) => Doctor.fromMap(doc.data)).toList();
+
+      setState(() {
+        this.doctorsList = doctors;
+      });
+    });
+
+    /// Fill out doctorNames List
+    doctorsList.forEach((doctor) =>
+        doctorNamesList.add(doctor.firstName + ' ' + doctor.lastName));
+  }
+
+  @override
+  void dispose() {
+    doctorsSubscription?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,11 +72,43 @@ class _AppointmentDialog extends State<AppointmentDialog> {
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            // Student Select
             StudymateTextField(
               labelText: 'Description',
               textEditingController: descriptionController,
               validation: 'name',
               icon: Icon(Icons.description, color: Colors.grey),
+            ),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide(),
+                  ),
+                  contentPadding:
+                      EdgeInsetsDirectional.fromSTEB(20.0, 10.0, 20.0, 10.0),
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.only(left: 5.0),
+                    child: Icon(Icons.directions_run, color: Colors.grey),
+                  )),
+              value: doctorName,
+              hint: Text('Select Doctor'),
+              items: doctorNamesList
+                  .map((label) => DropdownMenuItem(
+                        child: Text(label),
+                        value: label,
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  doctorName = value;
+                });
+              },
+            ),
+
+            // Button Actions
+            Row(
+              children: <Widget>[],
             ),
           ],
         ),
